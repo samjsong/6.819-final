2430f8a4d5954687d1eac59d3fb1d294f106af18
Veronica Lee <vslee@mit.edu>
2016-12-10 19:17:45 -0500

    generates result files

--- miniplacesCNN/run_miniplacesCNN.m
+++ miniplacesCNN/run_miniplacesCNN.m
@@ -7,40 +7,51 @@
 
 run matconvnet-1.0-beta23/matlab/vl_setupnn.m ;
 
+% data selection param
+data_selection = 'test'; % val or test
+
 % load pre-trained model
 load('categoryIDX.mat');
 path_model = 'refNet1-epoch-60.mat';
 load([path_model]) ;
 
-% load and preprocess an image
-image_path = '../final_project_data/images/';
-current_image = 'val/00000001.jpg';
-im = imread(strcat(image_path, current_image)) ;
-im_resize = imresize(im, net.normalization.imageSize(1:2)) ;
-im_ = single(im_resize) ; 
-for i=1:3
-    im_(:,:,i) = im_(:,:,i)-net.normalization.averageImage(i);
-end
-
 % change the last layer of CNN from softmaxloss to softmax
+net_norm = net.normalization;
 net.layers{1,end}.type = 'softmax';
 net.layers{1,end}.name = 'prob';
-
-% run the CNN
 net = vl_simplenn_tidy(net);
-res = vl_simplenn(net, im_) ;
 
-scores = squeeze(gather(res(end).x)) ;
-[score_sort, idx_sort] = sort(scores,'descend') ;
-figure, imagesc(im_resize) ;
+% open results.txt file
+fileID = fopen(strcat('results_',data_selection,'.txt'),'w');
 
-fileID = fopen('results.txt','w');
+% load and preprocess an image
+image_path = '../final_project_data/images/';
+files = dir(strcat('../final_project_data/images/',data_selection,'/*.jpg'));
 
-fprintf(fileID, '%s ', current_image);
+for file = files'
+    current_image = strcat(data_selection,'/',file.name);
+    im = imread(strcat(image_path, current_image)) ;
+    im_resize = imresize(im, net_norm.imageSize(1:2)) ;
+    im_ = single(im_resize) ; 
+    for i=1:3
+        im_(:,:,i) = im_(:,:,i)-net_norm.averageImage(i);
+    end
 
-for i=1:5
-    disp(sprintf('%s (%d), score %.3f', categoryIDX{idx_sort(i),1}, idx_sort(i), score_sort(i)));
-    fprintf(fileID, '%d ', idx_sort(i));
-end
+    % run the CNN
+    res = vl_simplenn(net, im_) ;
 
+    scores = squeeze(gather(res(end).x)) ;
+    [score_sort, idx_sort] = sort(scores,'descend') ;
+%     figure, imagesc(im_resize) ;
+
+    % prints image name and categories to results.txt
+    fprintf(fileID, '%s ', current_image);
+    for i=1:5
+%         disp(sprintf('%s (%d), score %.3f', categoryIDX{idx_sort(i),1}, idx_sort(i), score_sort(i)));
+        fprintf(fileID, '%d ', idx_sort(i)-1);
+    end
+    fprintf(fileID, '\n');
+end    
+
+% close results.txt file
 fclose(fileID);
